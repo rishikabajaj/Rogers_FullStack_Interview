2b84763cb004c233e784ded8e375b69c2413bb4d
Rishika Bajaj <37467301+rishikabajaj@users.noreply.github.com>
2022-11-21 22:40:15 -0500

    Update page-template-collection.component.ts

--- graphql/src/app/page-template-collection/page-template-collection.component.ts
+++ graphql/src/app/page-template-collection/page-template-collection.component.ts
@@ -5,16 +5,22 @@ import {DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser'
 @Component({
   selector: "app-page-template-collection",
   template: `<a [href]="downloadJsonHref" class="button" download="download.json"><b>Download Transformed JSON File</b></a>
-  <p>{{cat_array3}}</p>
+  <br>
+  <br>
   <ng-container *ngFor="let newJ of newJson">
+  <div class="boxed">
+
+  <br>
+
     <article *ngIf="newJ">
-      <br>
-      <br>
-      <a [href]="newJ.url">{{ newJ.seo.title }}</a>
-      <h2>Description</h2>
+      <a href={{newJ.url}}>{{ newJ.seo.title }}</a>
+
       <p>{{ newJ.seo.description }}</p>
-      <h2>isNoIndex</h2>
+
     </article>
+    </div>
+    <br>
+    <br>
   </ng-container>`,
 
   styleUrls: ["./page-template-collection.component.css"],
@@ -25,10 +31,7 @@ export class PageTemplateCollectionComponent implements OnInit {
   public persons: any = {};
   public newJson: any[]
   public cat_array: any[];
-  public cat_array2: any = [];
-  public cat_array3: any[];
   public detailList1 : any;
-  public cat = "";
 
   constructor(private pageTemplatesGQL: PageTemplatesGQL,private sanitizer: DomSanitizer) {
     this.ngOnInit();
@@ -39,7 +42,12 @@ export class PageTemplateCollectionComponent implements OnInit {
     this.pageTemplates = (data.pageTemplateCollection?.items);
     const jsonData = JSON.stringify(this.pageTemplates,function(key,value) {
       if (key=="url"){
-        return value.replace("/home","https://www.rogers.com")
+        if (value.startsWith('/home')){
+          return value.replace("/home","https://www.rogers.com")
+        }else{
+          return value.replace(value, "https://www.rogers.com"+value)
+        }
+        
       }if (key=="seo"){
         return {"title":value.title.replace(" - Rogers", " | Rogers"),
                 "description":value.description.substring(0, 80),
@@ -49,38 +57,33 @@ export class PageTemplateCollectionComponent implements OnInit {
         return value;
       }
     });
+
     const userData = JSON.parse(jsonData);
     const jsonData1 = JSON.stringify(userData, null, 2);
-    const stringify1 = JSON.parse(jsonData1);
-    for (var i = 0; i < stringify1.length; i++) {
-      if (stringify1[i]['url'].startsWith('https://www.rogers.com')){
-        var newest = stringify1[i]['url'].split("/");
+    const stringifiedData = JSON.parse(jsonData1);
+    for (var i = 0; i < stringifiedData.length; i++) {
+      if (stringifiedData[i]['url'].startsWith('https://www.rogers.com')){
+        var newest = stringifiedData[i]['url'].split("/");
         for (var j = 0; j< newest.length-3;j++) {
           var upper = newest[j+3].charAt(0).toUpperCase( ) + newest[j+3].substring(1,)
-          this.persons[j+1] = stringify1[i]['url'].replace(newest[j+3],upper);
-          stringify1[i]['category']=this.persons;
+          this.persons[j+1] = stringifiedData[i]['url'].replace(newest[j+3],upper);
+          stringifiedData[i]['category']=this.persons;
         }
-        this.cat_array2.push(this.persons)
-        this.cat += JSON.stringify(this.persons);
         this.persons={}; 
       }else{
-        stringify1[i]['category']='null';
+        stringifiedData[i]['category']={};
       }
     }
+    this.newJson = stringifiedData;
+    console.log(this.newJson)
+
+    const finalJsonData = JSON.stringify(stringifiedData, null, 2);
     
-    this.cat_array = this.cat.split("}");
-    this.cat_array3 = JSON.parse(JSON.stringify(this.cat_array2))
-    this.newJson = stringify1;
-    const jsonData2 = JSON.stringify(stringify1, null, 2);
-    console.log(jsonData2);
-    
+    //download JSON File
     var url = this.sanitizer.bypassSecurityTrustUrl(
-      "data:application/json;charset=UTF-8," + encodeURIComponent(jsonData2)
+      "data:application/json;charset=UTF-8," + encodeURIComponent(finalJsonData)
     );
     this.downloadJsonHref = url;
     });
   }
 }
-
-
-
